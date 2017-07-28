6e6706fd3a8008854baa7683af53de4ca34f339d
Miguel Cazares <miguel.cazares@shipstation.com>
2017-07-27 20:53:51 -0500

    add bridging header

---
+++ Foodly/Foodly-Bridging-Header.h
@@ -0,0 +1,3 @@
+#import <LHSCategoryCollection/UIView+LHSAdditions.h>
+#import <LHSCategoryCollection/UIApplication+LHSAdditions.h>
+#import <SVProgressHUD/SVProgressHUD.h>
--- Foodly/SearchViewController.swift
+++ Foodly/SearchViewController.swift
@@ -19,7 +19,6 @@ class SearchViewController: UIViewController {
         
         let searchBar = UISearchBar()
         searchBar.translatesAutoresizingMaskIntoConstraints = false
-        
     }
 }
 
