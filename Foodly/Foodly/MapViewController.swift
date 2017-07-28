0c1a51a590492ac30f00903275770a628a3827f7
Miguel Cazares <miguel.cazares@shipstation.com>
2017-07-27 19:31:14 -0500

    initial project setup

---
+++ Foodly/Foodly/MapViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MapViewController.swift
+//  Foodly
+//
+//  Created by Miguel Cazares on 7/27/17.
+//  Copyright © 2017 Cazares. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
